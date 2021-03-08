b155dbbd4efc400bf28e3e3180a4b3ae69cc29be
Mohammad Nofal <m7madmody@gmail.com>
2021-03-08 17:58:46 +0200

    MohammadNofal edit

--- js/donate.js
+++ js/donate.js
@@ -114,3 +114,19 @@ shelterForm.addEventListener('submit', function (event) {
 if (JSON.parse(localStorage.getItem('shelter'))) {
   shelterAll = JSON.parse(localStorage.getItem('shelter'));
 }
+
+
+function openTab (evt, tabName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName('tabcontent');
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+// document.getElementById("openTab").click();
