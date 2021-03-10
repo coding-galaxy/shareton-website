9f1b359ef8fe165f75938ab58ffb6bd92c332019
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-03-10 10:36:45 +0200

    fixing styles for home page

--- js/home.js
+++ js/home.js
@@ -14,36 +14,41 @@ const section2 = document.querySelector('.grid-container');
 
 const donate = document.querySelector('.donate');
 const section3 = document.querySelector('.arrow__conatainer3');
+const faLogo = document.getElementById('logo-fa');
+const logoSpan = document.querySelector('.logospan');
+const h1Logo = document.querySelector('.h1logo');
 
-const slides = document.getElementsByClassName('.client-container');
+const hover1 = document.querySelector('.hover1');
+const hover2 = document.querySelector('.hover2');
+// const hover3 = document.querySelector('.hover3');
 
-section1.addEventListener('click', function (event) {
-  if (event.target.matches('.arrow')) {
-    ourSection.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'start',
-    });
-  }
-});
-section2.addEventListener('click', function (event) {
-  if (event.target.matches('.arrow')) {
-    doSection.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'start',
-    });
-  }
-});
-section3.addEventListener('click', function (event) {
-  if (event.target.matches('.arrow')) {
-    goalsSection.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'start',
-    });
-  }
-});
+// section1.addEventListener('click', function (event) {
+//   if (event.target.matches('.arrow')) {
+//     ourSection.scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start',
+//       inline: 'start',
+//     });
+//   }
+// });
+// section2.addEventListener('click', function (event) {
+//   if (event.target.matches('.arrow')) {
+//     doSection.scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start',
+//       inline: 'start',
+//     });
+//   }
+// });
+// section3.addEventListener('click', function (event) {
+//   if (event.target.matches('.arrow')) {
+//     goalsSection.scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start',
+//       inline: 'start',
+//     });
+//   }
+// });
 
 contact.addEventListener('click', function (event) {
   console.log(event.target);
@@ -64,16 +69,36 @@ window.onscroll = function () {
     document.getElementById('navbar').style.top = 0;
     navItem.classList.add('scrolled');
     donate.classList.add('scrolled-donate');
+    faLogo.classList.add('logo-scrolled');
+    logoSpan.classList.add('heading-scrolled');
+    h1Logo.classList.add('h1-scrolled');
+
+    hover1.classList.add('hover-scrolled');
+    hover2.classList.add('hover-scrolled');
+    // hover3.classList.add('hover-scrolled');
   } else {
     document.getElementById('navbar').style.top = '-70px';
     navItem.classList.remove('scrolled');
     donate.classList.remove('scrolled-donate');
+    logoSpan.classList.remove('heading-scrolled');
+    faLogo.classList.remove('logo-scrolled');
+    h1Logo.classList.remove('h1-scrolled');
+
+    hover1.classList.remove('hover-scrolled');
+    hover2.classList.remove('hover-scrolled');
+    // hover3.classList.remove('hover-scrolled');
   }
   startYOffset = currentYOffset;
 
   if (currentYOffset === 0) {
     navItem.classList.remove('scrolled');
     donate.classList.remove('scrolled-donate');
+    faLogo.classList.remove('logo-scrolled');
+    logoSpan.classList.remove('heading-scrolled');
+    h1Logo.classList.remove('h1-scrolled');
+    hover1.classList.remove('hover-scrolled');
+    hover2.classList.remove('hover-scrolled');
+    // hover3.classList.remove('hover-scrolled');
     // navItem.classList.add('initial');
   }
 };
@@ -102,7 +127,7 @@ let textIndex = 0;
 textSlides();
 function textSlides() {
   let texts = document.getElementsByClassName('glass');
-  let dots = document.getElementsByClassName('dot1');
+
   for (let index = 0; index < texts.length; index++) {
     texts[index].style.display = 'none';
   }
@@ -110,19 +135,8 @@ function textSlides() {
   if (textIndex > texts.length) {
     textIndex = 1;
   }
-  for (let index = 0; index < dots.length; index++) {
-    dots[index].className = dots[index].className.replace(' active1', '');
-  }
+
   texts[textIndex - 1].style.display = 'flex';
-  dots[textIndex - 1].className += ' active1';
+
   setTimeout(textSlides, 3500);
 }
-
-let section1ScrollIn = ourSection.getBoundingClientRect();
-
-window.onscroll = function () {
-  console.log(window.pageYOffset);
-  // console.log('hi');
-  let x = window.pageYOffset;
-  if (x === section1ScrollIn) console.log('hello');
-};
